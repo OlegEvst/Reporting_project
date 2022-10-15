09a8c3c167b8b1fac0dddfe823da06d1bb0a1566
oleg_evstifeev <evstifeev96@yandex.ru>
2022-10-15 23:14:36 +0300

    Добавили часть решения итогового проекта

--- Reporting_project_evstifeev/Reporting_project_evstifeev.cs
+++ Reporting_project_evstifeev/Reporting_project_evstifeev.cs
@@ -2,4 +2,35 @@
 длинна которых меньше или равна 3 символа. Первоначальный массив можно ввести ввести с 
 клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться 
 коллекциями, лучше обойтись исключительно массивами. */
- 
+
+Console.WriteLine("Введите любые символы через ','");
+
+string[] values = Console.ReadLine().Split(new char[] { ',' });
+
+string[] GetArray(string[] inputArray)
+{
+    string[] array = new string[inputArray.Length];
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(inputArray[i].Length <= 3)
+        {
+            array[i] = inputArray[i]; 
+        }
+    }
+    return array;
+}
+
+void PrintArray(string[] printArr)
+{
+        for(int l = 0; l < printMx.GetLength(1); l++) 
+        {
+        Console.Write($"[");
+        Console.Write($"{printMx[k,l]}, ");
+        Console.Write($"\b\b]");   
+        }
+        Console.WriteLine("");
+}
+
+
+GetArray(values);
